23df42b680bb6fcc4b6c04962666f7baedecbfa1
mattcav <matteo@mced.it>
2014-03-27 17:04:25 +0100

    assemble setup + inuit.css

---
+++ Gruntfile.js
@@ -0,0 +1,137 @@
+/*
+ * Generated on 2014-03-27
+ * generator-assemble v0.4.11
+ * https://github.com/assemble/generator-assemble
+ *
+ * Copyright (c) 2014 Hariadi Hinta
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// '<%= config.src %>/templates/pages/{,*/}*.hbs'
+// use this if you want to match all subfolders:
+// '<%= config.src %>/templates/pages/**/*.hbs'
+
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    config: {
+      src: 'src',
+      dist: 'dist'
+    },
+
+    sass: {
+      options: {
+        includePaths: ['<%= config.src %>/assets/scss/inuit']
+      },
+      dist: {
+        options: {
+          outputStyle: 'nested'
+        },
+        files: {
+          '<%= config.dist %>/assets/css/style.css': '<%= config.src %>/scss/style.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      single_file: {
+        options: {
+          browsers: ['last 2 version', 'ie 8', 'ie 7']
+        },
+        src: '<%= config.dist %>/assets/css/style.css',
+        dest: '<%= config.dist %>/assets/css/style.css'
+      },
+    },
+
+    watch: {
+      assemble: {
+        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
+        tasks: ['assemble']
+      },
+      sass: {
+        files: '<%= config.src %>/scss/{,*/}*.scss',
+        tasks: ['sass', 'autoprefixer']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.dist %>/{,*/}*.html',
+          '<%= config.dist %>/assets/{,*/}*.css',
+          '<%= config.dist %>/assets/{,*/}*.js',
+          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: false,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      }
+    },
+
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: '<%= config.dist %>/assets',
+          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          data: '<%= config.src %>/data/*.{json,yml}',
+          partials: '<%= config.src %>/templates/partials/*.hbs'
+        },
+        files: {
+          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+        }
+      }
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: ['<%= config.dist %>/**/*.{html,xml}']
+
+  });
+
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('server', [
+    'clean',
+    'assemble',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'assemble'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+# faustini
+
+> Studio Faustini website. 
+
+Featuring:
+
+## [Assemble](http://assemble.io/)
+## [Inuit.css](https://github.com/csswizardry/inuit.css/)
